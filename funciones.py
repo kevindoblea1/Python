67703a1aa8d45ef4b1ec3907236076a3d0725dc9
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-01-30 11:40:36 -0600

    se añadde cambios

---
+++ funciones.py
@@ -0,0 +1,8 @@
+def a(cualquier_parametro):
+    """descripcion de nuestra funcion
+    cualquier_parametro int cualquier entero
+    return cualquier parametro + 5"""
+    return cualquier_parametro + 5
+
+print(a(5))
+
